589e8a739a0231251052d991e5101862351d28f7
Marcus Christie <machrist@iu.edu>
2019-08-28 10:52:29 -0400

    Registered an image output view for gaussian log (just dummy scatterplot for now)

--- gateways19_tutorial/output_views.py
+++ gateways19_tutorial/output_views.py
@@ -1,8 +1,28 @@
+import io
+
+import numpy as np
+from matplotlib.figure import Figure
+
 class GaussianLogViewProvider:
     display_type = 'image'
     name = "Gaussian Log Viewer"
 
-    def generate_data(self, experiment_output, experiment, output_file=None):
-        # TODO: return dictionary with image data
+    def generate_data(self, request, experiment_output, experiment, output_file=None):
+        # return dictionary with image data
+        N = 500
+        x = np.random.rand(N)
+        y = np.random.rand(N)
+        fig = Figure()
+        ax = fig.subplots()
+        ax.scatter(x, y)
+        ax.set_title('Random scatterplot')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        buffer = io.BytesIO()
+        fig.savefig(buffer, format='png')
+        image_bytes = buffer.getvalue()
+        buffer.close()
         return {
+            'image': image_bytes,
+            'mime-type': 'image/png'
         }
--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ setuptools.setup(
     description="Custom output viewer and django app for Gateways 2019 Tutorial",
     packages=setuptools.find_packages(),
     install_requires=[
-        'django>=1.11.16'
+        'django>=1.11.16',
+        'numpy',
+        'matplotlib'
     ],
     entry_points="""
+[airavata.output_view_providers]
+gaussian-log-image = gateways19_tutorial.output_views:GaussianLogViewProvider
 """,
 )
