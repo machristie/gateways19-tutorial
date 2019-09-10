afa196a89e9178f4e89607cae1f025a1198f3c26
Marcus Christie <machrist@iu.edu>
2019-09-10 11:37:23 -0400

    Adding simple "Hello World" Django app

---
+++ gateways19_tutorial/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class Gateways19TutorialAppConfig(AppConfig):
+    name = 'gateways19_tutorial'
+    label = name
+    verbose_name = "Gateways 19 Tutorial"
+    fa_icon_class = "fa-comment"
--- setup.py
+++ setup.py
@@ -13,5 +13,7 @@ setuptools.setup(
     entry_points="""
 [airavata.output_view_providers]
 gaussian-log-image = gateways19_tutorial.output_views:GaussianLogViewProvider
+[airavata.djangoapp]
+gateways19_tutorial = gateways19_tutorial.apps:Gateways19TutorialAppConfig
 """,
 )
