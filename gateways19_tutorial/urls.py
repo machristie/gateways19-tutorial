004f2f5e5fe8d60545cbe44ad5ca58fd8913fc77
Marcus Christie <machrist@iu.edu>
2019-09-18 12:10:14 -0400

    Resetting the state of the repo for tutorial

--- gateways19_tutorial/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-
-from . import views
-
-app_name = 'gateways19_tutorial'
-urlpatterns = [
-    url(r'^hello/', views.hello_world, name="home"),
-    url(r'^languages/', views.languages, name="languages"),
-]
