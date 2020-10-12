3eef0be573eb3bdf8ee3bc0de8fbc08a572491f3
Marcus Christie <machrist@iu.edu>
2020-10-12 09:27:11 -0400

    Rename

--- gateways19_tutorial/apps.py
+++ gateways19_tutorial/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class Gateways19TutorialAppConfig(AppConfig):
     name = 'gateways19_tutorial'
     label = name
-    verbose_name = "Gateways 19 Tutorial"
+    verbose_name = "Gateways Tutorial"
     fa_icon_class = "fa-comment"
